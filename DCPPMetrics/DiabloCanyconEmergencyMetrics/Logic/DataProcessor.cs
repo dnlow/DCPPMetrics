74f88037c4c671829ead3f61919dd2b870e08a5b
Dane Low <dnlow@live.com>
2018-02-23 15:31:06 -0800

    Implemented data processing

--- DCPPMetrics/DiabloCanyconEmergencyMetrics/Logic/DataProcessor.cs
+++ DCPPMetrics/DiabloCanyconEmergencyMetrics/Logic/DataProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -32,42 +33,76 @@ namespace DiabloCanyconEmergencyMetrics.Logic
          * MT8 = Davis Peak
          * MT9 = Grover Beach
          * 
+         * { 
+         *      "MT1" : 
+         *              {
+         *                  "0:00" : {
+         *                                  "WindSpeed" : 4,
+         *                                  "WindDirection" : 123.5
+         *              }
+         *      }
+         * }
+         * 
+         * 
+         * 
          */
-        private Dictionary<string, Dictionary<string, Dictionary<string, float>>> ReadCSVFile()
+
+        public Dictionary<string, Dictionary<string, Dictionary<string, string>>> ReadCSVFile()
         {
-            Dictionary<string, Dictionary<string, Dictionary<string, float>>> metrics = 
-                new Dictionary<string, Dictionary<string, Dictionary<string, float>>>();
+            Dictionary<string, Dictionary<string, Dictionary<string, string>>> metrics =
+                new Dictionary<string, Dictionary<string, Dictionary<string, string>>>();
+
+            #region Measurements
+            // Primary Measurements
+            Dictionary<string, Dictionary<string, string>> MT1Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Seconday Measurements
+            Dictionary<string, Dictionary<string, string>> MT2Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Point Buchon Measurements
+            Dictionary<string, Dictionary<string, string>> MT3Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Los Osos Cemetery Measurements
+            Dictionary<string, Dictionary<string, string>> MT4Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Foot Hill Measurements
+            Dictionary<string, Dictionary<string, string>> MT5Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Service Center Measurements
+            Dictionary<string, Dictionary<string, string>> MT6Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Energy Edu.Center Measurements
+            Dictionary<string, Dictionary<string, string>> MT7Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Davis Peak Measurements
+            Dictionary<string, Dictionary<string, string>> MT8Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+
+            // Grover Beach Measurements
+            Dictionary<string, Dictionary<string, string>> MT9Measurements =
+                            new Dictionary<string, Dictionary<string, string>>();
+            #endregion
+
+            Debug.Print("Assigning Column Rows");
 
             using (var reader = new StreamReader(fileName))
             {
-               /* 
-                * Read header row & assign column #s for each location's measurements
-                * 
-                * MT(1-2)_1 = Wind Direction
-                * MT(1-2)_3 = Wind Speed
-                *
-                * MT(3-9)_4 = Wind Speed
-                * MT(3-9)_5 = Wind Direction
-                */
-
-                int MT1_1 = 0;
-                int MT1_3 = 0;
-                int MT2_1 = 0;
-                int MT2_2 = 0;
-                int MT3_4 = 0;
-                int MT3_5 = 0;
-                int MT4_4 = 0;
-                int MT4_5 = 0;
-                int MT5_4 = 0;
-                int MT5_5 = 0;
-                int MT6_4 = 0;
-                int MT6_5 = 0;
-                int MT7_4 = 0;
-                int MT7_5 = 0;
-                int MT8_4 = 0;
-                int MT8_5 = 0;
-                int MT9_4 = 0;
-                int MT9_5 = 0;
+                /* 
+                 * Read header row & assign column #s for each location's measurements
+                 * 
+                 * MT(1-2)_1 = Wind Direction
+                 * MT(1-2)_3 = Wind Speed
+                 *
+                 * MT(3-9)_4 = Wind Speed
+                 * MT(3-9)_5 = Wind Direction
+                 */
 
                 // read header row
                 var headerRow = reader.ReadLine();
@@ -75,47 +110,110 @@ namespace DiabloCanyconEmergencyMetrics.Logic
                 var headerValues = headerRow.Split(',');
                 // Find the column number for each measurement
 
-                for (int i = 0; i < headerValues.Length; i++)
-                {
-                    if (headerValues[i].Equals("MT1_1")) MT1_1 = i;
-                    else if (headerValues[i].Equals("MT1_3")) MT1_3 = i;
-                    else if (headerValues[i].Equals("MT2_1")) MT2_1 = i;
-                    else if (headerValues[i].Equals("MT2_2")) MT2_2 = i;
-                    else if (headerValues[i].Equals("MT3_4")) MT3_4 = i;
-                    else if (headerValues[i].Equals("MT3_5")) MT3_5 = i;
-                    else if (headerValues[i].Equals("MT4_4")) MT4_4 = i;
-                    else if (headerValues[i].Equals("MT4_5")) MT4_5 = i;
-                    else if (headerValues[i].Equals("MT5_4")) MT5_4 = i;
-                    else if (headerValues[i].Equals("MT5_5")) MT5_5 = i;
-                    else if (headerValues[i].Equals("MT6_4")) MT6_4 = i;
-                    else if (headerValues[i].Equals("MT6_5")) MT6_5 = i;
-                    else if (headerValues[i].Equals("MT7_4")) MT7_4 = i;
-                    else if (headerValues[i].Equals("MT7_5")) MT7_5 = i;
-                    else if (headerValues[i].Equals("MT8_4")) MT8_4 = i;
-                    else if (headerValues[i].Equals("MT8_5")) MT8_5 = i;
-                    else if (headerValues[i].Equals("MT9_4")) MT9_4 = i;
-                    else if (headerValues[i].Equals("MT9_5")) MT9_5 = i;
-                }
+                int MT1_1 = GetDataColumns(headerValues, "MT1_1");
+                int MT1_3 = GetDataColumns(headerValues, "MT1_3");
+                int MT2_1 = GetDataColumns(headerValues, "MT2_1");
+                int MT2_3 = GetDataColumns(headerValues, "MT2_3");
+                int MT3_4 = GetDataColumns(headerValues, "MT3_4");
+                int MT3_5 = GetDataColumns(headerValues, "MT3_5");
+                int MT4_4 = GetDataColumns(headerValues, "MT4_4");
+                int MT4_5 = GetDataColumns(headerValues, "MT4_5");
+                int MT5_4 = GetDataColumns(headerValues, "MT5_4");
+                int MT5_5 = GetDataColumns(headerValues, "MT5_5");
+                int MT6_4 = GetDataColumns(headerValues, "MT6_4");
+                int MT6_5 = GetDataColumns(headerValues, "MT6_5");
+                int MT7_4 = GetDataColumns(headerValues, "MT7_4");
+                int MT7_5 = GetDataColumns(headerValues, "MT7_5");
+                int MT8_4 = GetDataColumns(headerValues, "MT8_4");
+                int MT8_5 = GetDataColumns(headerValues, "MT8_5");
+                int MT9_4 = GetDataColumns(headerValues, "MT9_4");
+                int MT9_5 = GetDataColumns(headerValues, "MT9_5");
 
+                #region Measurement Populating
                 // read data lines           
                 while (!reader.EndOfStream)
                 {
-                    Dictionary<string, Dictionary<string, float>> measurements = new Dictionary<string, Dictionary<string, float>>();
-                    string stationName = "";
-
                     var line = reader.ReadLine();
                     var values = line.Split(',');
 
+                    // Get Time Stamp
+                    string timeStamp = values[0];
+
+                    // Add MT1 Data
+                    MT1Measurements.Add(timeStamp, GetMeasurementData(values, MT1_3, MT1_1));
+
+                    // Add MT2 Data
+                    MT2Measurements.Add(timeStamp, GetMeasurementData(values, MT2_3, MT2_1));
+
+                    // Add MT3 Data
+                    MT3Measurements.Add(timeStamp, GetMeasurementData(values, MT3_4, MT3_5));
 
 
-                    metrics.Add(stationName, measurements);
+                    // Add MT4 Data
+                    MT4Measurements.Add(timeStamp, GetMeasurementData(values, MT4_4, MT4_5));
+
+                    // Add MT5 Data
+                    MT5Measurements.Add(timeStamp, GetMeasurementData(values, MT5_4, MT5_5));
+
+                    // Add MT6 Data
+                    MT6Measurements.Add(timeStamp, GetMeasurementData(values, MT6_4, MT6_5));
+
+                    // Add MT7 Data
+                    MT7Measurements.Add(timeStamp, GetMeasurementData(values, MT7_4, MT7_5));
+
+                    // Add MT8 Data
+                    MT8Measurements.Add(timeStamp, GetMeasurementData(values, MT8_4, MT8_5));
+
+                    // Add MT9 Data
+                    MT9Measurements.Add(timeStamp, GetMeasurementData(values, MT9_4, MT9_5));
                 }
+
+                metrics.Add("MT1", MT1Measurements);
+                metrics.Add("MT2", MT2Measurements);
+                metrics.Add("MT3", MT3Measurements);
+                metrics.Add("MT4", MT4Measurements);
+                metrics.Add("MT5", MT5Measurements);
+                metrics.Add("MT6", MT6Measurements);
+                metrics.Add("MT7", MT7Measurements);
+                metrics.Add("MT8", MT8Measurements);
+                metrics.Add("MT9", MT9Measurements);
+                #endregion
             }
             return metrics;
         }
 
+        private int GetDataColumns(string[] headerValues, string columnName)
+        {
+            for (int i = 0; i < headerValues.Length; i++)
+            {
+                if (headerValues[i].Equals(columnName)) return i;
+            }
+            return 0;
+        }
 
+        private Dictionary<string, string> GetMeasurementData(string[] values, int windSpeed, int windDirection)
+        {
+            Dictionary<string, string> measurements = new Dictionary<string, string>()
+            {
+                { "WS", values[windSpeed] },
+                { "WD", values[windDirection] }
+            };
 
+            return measurements;
+        }
+
+        private void PrintMeasurements(Dictionary<string, Dictionary<string, string>> measurements)
+        {
+            foreach (KeyValuePair<string, Dictionary<string, string>> pair in measurements)
+            {
+                Debug.Print(pair.Key);
+                foreach (KeyValuePair<string, string> measurement in pair.Value)
+                {
+                    Debug.Print(measurement.Key);
+                    Debug.Print(measurement.Value);
+                }
+            }
+        }
 
     }
 }
